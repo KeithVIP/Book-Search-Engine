bcf6f4b8bce5c9366207205df91645b99ee00577
Keith <keithvip@gmail.com>
2022-07-20 17:18:06 -0500

    resolvers added

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
+const bookSchema = require('./Book');
 
 // import schema from Book.js
 const bookSchema = require('./Book');
