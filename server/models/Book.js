369a29b2f17ebb67f6207cae47f53b951c3f75be
Keith <keithvip@gmail.com>
2022-07-20 16:28:12 -0500

    schemas created and added to models

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,4 +1,5 @@
 const { Schema } = require('mongoose');
+const { Schema } = mongoose;
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const bookSchema = new Schema({
@@ -11,20 +12,20 @@ const bookSchema = new Schema({
     type: String,
     required: true,
   },
-  // saved book id from GoogleBooks
-  bookId: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
   title: {
     type: String,
     required: true,
+    // saved book id from GoogleBooks
+    bookId: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    link: {
+      type: String,
+    },
   },
 });
 
