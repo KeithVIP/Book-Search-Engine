6ca873fdf2815fe05054d0f1c4ec2e50db434bfb
Keith <keithvip@gmail.com>
2022-07-20 14:01:04 -0500

    auth and server updated

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -16,7 +16,8 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
+      // res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
@@ -24,16 +25,18 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
+      console.error(err);
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // console.log('Invalid token');
+      // return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
-    next();
+    // next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
